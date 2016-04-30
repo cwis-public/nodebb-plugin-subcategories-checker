7f42cdc34e5cc5cdb252129fa3b52bc79196af4c
Christopher Allène <christopher.allene@piximedia.fr>
2016-04-30 18:41:29 +0200

    Fix callbacks

--- index.js
+++ index.js
@@ -5,14 +5,18 @@
 var fs = require("fs");
 
 exports.addRoute = function(custom_routes, callback) {
+	console.log("Adding routes");
 	var templateCategory = fs.readFileSync(path.resolve(__dirname, '../../public/templates/category.tpl'));
+	console.log(templateCategory);
 	templateCategory += "<script> alert('hello'); </script>";
+	console.log(custom_routes);
 	custom_routes.templates.push({ template: "category.tpl", content: templateCategory });
 	return callback(null, custom_routes);
 };
 
 exports.addScripts = function(scripts, callback) {
-	return scripts.concat(['plugins/nodebb-plugin-subcategories-checker/js/subcategories-checker.js']);
+	scripts.concat(['plugins/nodebb-plugin-subcategories-checker/js/subcategories-checker.js']);
+	return callback(null, scripts);
 };
 
 })();
