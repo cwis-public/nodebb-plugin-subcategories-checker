310f5dc276ca7c8e47f659847a9f9d6590f1ae1e
Christopher Allène <christopher.allene@piximedia.fr>
2016-04-30 18:38:49 +0200

    Fix callback

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.addRoute = function(custom_routes, callback) {
 	var templateCategory = fs.readFileSync(path.resolve(__dirname, '../../public/templates/category.tpl'));
 	templateCategory += "<script> alert('hello'); </script>";
 	custom_routes.templates.push({ template: "category.tpl", content: templateCategory });
+	return callback(null, custom_routes);
 };
 
 exports.addScripts = function(scripts, callback) {
