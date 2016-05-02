39749e976eef468328fda9e5f3da2b35b93d310d
Christopher Allène <christopher.allene@piximedia.fr>
2016-05-02 23:51:40 +0200

    Improve support when using cluster

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ var fs = require("fs"),
 
 exports.init = function(params, callback) {
 	var file = path.resolve(__dirname, '../../public/templates/category.tpl');
-	var templateCategory = fs.readFileSync(file, { encoding: "utf-8" });
-	templateCategory +=" \n<script>window.resyncSubcategoriesChecker()</script>\n";
-	fs.writeFileSync(file, templateCategory);
-	return callback();
+	
+	var doit = function() {
+		try {
+			var templateCategory = fs.readFileSync(file, { encoding: "utf-8" });
+			if(!templateCategory.match(/resyncSubcategoriesChecker/)) {
+				templateCategory +=" \n<script>window.resyncSubcategoriesChecker()</script>\n";
+				fs.writeFileSync(file, templateCategory);
+			}
+		} catch(e) {
+			console.log("Caught error loading category.tpl: " + e);
+			return setTimeout(doit, 100);
+		}
+		console.log("Success overriding category.tpl");
+		return callback();
+	};
+	doit();
 };
 
 exports.addScripts = function(scripts, callback) {
